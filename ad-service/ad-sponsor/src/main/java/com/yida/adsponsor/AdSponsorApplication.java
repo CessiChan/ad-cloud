2bdc9728511f8c522e4da8b89cec57b0d6285321
CessiChan <si_chan@163.com>
2019-09-25 09:58:29 +0800

    后台服务pom文件修改

---
+++ ad-service/ad-sponsor/src/main/java/com/yida/adsponsor/AdSponsorApplication.java
@@ -0,0 +1,13 @@
+package com.yida.adsponsor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdSponsorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdSponsorApplication.class, args);
+    }
+
+}
